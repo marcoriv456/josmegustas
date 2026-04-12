fd232ec02d2526c00a50dffbcd27b416540a6669
marcoriv456 <marcosnicolasrivera456@gmail.com>
2026-04-11 19:39:37 -0600

    mensaje medio añadido

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { AfterViewInit, Component, signal, viewChild } from '@angular/core';
 import { Flor } from './ui/flor/flor';
 import { Gradient } from './ui/gradient/gradient';
 import { Grass } from './ui/grass/grass';
+import { Message1 } from './ui/message-1/message-1';
 import { Particles } from './ui/particles/particles';
 
 @Component({
   selector: 'app-root',
-  imports: [Flor, Grass, Particles, Gradient],
+  imports: [Flor, Grass, Particles, Gradient, Message1],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
@@ -15,6 +16,8 @@ export class App implements AfterViewInit {
 
   readonly flor = viewChild(Flor);
   readonly grass = viewChild(Grass);
+
+  active = signal(1);
 
   ngAfterViewInit(): void {
     this.grass()?.start();
---
+++ src/app/ui/message-1/message-1.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-1',
+  imports: [],
+  templateUrl: './message-1.html',
+  styleUrl: './message-1.scss',
+})
+export class Message1 {}
